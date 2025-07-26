9503d29108496a6a5ccef71345ddb5b1b538c83c
CookieBox26 <cookie-box@cookie-box.info>
2025-07-26 14:16:36 +0900

    Update site

--- register.py
+++ register.py
@@ -1,4 +1,23 @@
-import shutil
+import base64
+import os
+import re
+
+
+def copy(target, file_0, file_1):
+    with (
+        open(file_0, mode='r') as ifile, 
+        open(file_1, mode='w', encoding='utf-8', newline='\n') as ofile,
+    ):
+        for line in ifile:
+            match = re.search(r'<img src="([^"]+)"', line)
+            if not match:
+                ofile.write(line)
+                continue
+            img_path = os.path.join(target, match.group(1))
+            with open(img_path, 'rb') as image_file:
+                base64_img = base64.b64encode(image_file.read()).decode('utf-8')
+            line_ = line.replace(match.group(1), f'data:image/png;base64, {base64_img}')
+            ofile.write(line_)
 
 
 def main():
@@ -6,13 +25,11 @@ def main():
     targets = [
         'outputs/sample_mini',
     ]
-    results = []
     for target in targets:
-        file_0 = target + '/report.html'
-        file_1 = target.split('/')[-1] + '.html'
-        shutil.copy(file_0, file_1)
+        file_0 = os.path.join(target, 'report.html')
+        file_1 = os.path.basename(target) + '.html'
+        copy(target, file_0, file_1)
         ofile.write(f'- [{file_1}]({file_1})\n')
-        results.append(file_1)
     ofile.close()
 
 
