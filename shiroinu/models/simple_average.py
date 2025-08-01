923228ba9fb4ac7d5520e0130ddacf39eef2404e
CookieBox26 <cookie-box@cookie-box.info>
2025-08-01 05:28:27 +0000

    Bugfix

--- shiroinu/models/simple_average.py
+++ shiroinu/models/simple_average.py
@@ -25,7 +25,10 @@ class SimpleAverage(BaseModel):
         # -> batch_size, n_period, period_len, num_of_roads
         x_view = x.view(batch_size, -1, self.period_len, num_of_roads)
         n_period = x_view.shape[1]
-        w = torch.tensor([self.decay_rate**i for i in reversed(range(n_period))], dtype=torch.float)
+        w = torch.tensor(
+            [self.decay_rate**i for i in reversed(range(n_period))],
+            dtype=torch.float, device=self.device,
+        )
         w = w / w.sum()
         return torch.einsum('j,ijkl->ikl', (w, x_view))
 
